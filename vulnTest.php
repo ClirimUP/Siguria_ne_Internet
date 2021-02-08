79ff0ee49d8382dc6b21fab51c9bab96858cb4f7
ClirimUP <57815861+ClirimUP@users.noreply.github.com>
2021-02-08 12:17:36 +0100

    Update vulnTest.php

--- vulnTest.php
+++ vulnTest.php
@@ -1,1 +1,38 @@
+&lt;?php
 
+
+// Creating a database connection, because sql injection starts with a database :) , Please note this database must exist on your server.
+
+$serverName = "localhost";
+$userName = "root";
+$password = "********";
+$databaseName = "socialnetwork";
+
+
+// this argument will be passed from our python script
+
+$userFromArg = $_POST['userid'];
+
+$dbcn = new mysqli($serverName, $userName, $password,$databaseName);
+
+if($dbcn-&gt;connect_error)
+{
+    die("Connection failed:". $dbcn-&gt;connect_error);
+}
+
+
+
+$sql = "SELECT userID, userName AS max FROM user where userID=$userFromArg";
+    $result = $dbcn-&gt;query($sql);
+
+    if ($result-&gt;num_rows &gt; 0) {
+        $row = $result-&gt;fetch_assoc();
+        $name = $row["userName"];
+    echo $name;
+    }else {
+        echo $fail;
+        exit();
+    }
+
+
+?&gt
